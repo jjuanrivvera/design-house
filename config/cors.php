365bc773aaee694c608551b26b20ca1e5aa77f10
Juan Felipe Rivera G <juan.rivera@alegra.com>
2021-12-28 19:03:49 -0500

    Testing job error in another VPS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.jjuanrivvera.com'],
+    // 'allowed_origins' => ['*.jjuanrivvera.com', '*.local'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
